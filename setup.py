b16bfbae741fe6bd10a83dc691a57f5bc50cc1c8
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-12-02 10:16:23 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Framework :: Pytest",
     ],

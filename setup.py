333d272a4a6dc37e648eef22224ccfb63332cf37
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-06-19 13:01:18 +0200

    Stable version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.rst", "r") as readme_file:
 
 setup(
     name="pytest-notification",
-    version="0.2.0-dev",
+    version="0.2.0",
     description="A pytest plugin for sending a desktop notification and "
                 "playing a sound upon completion of tests",
     author="Ruben Vorderman",
